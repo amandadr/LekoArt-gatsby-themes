844b9296105ec5b9d4fe9a27940213d12d8b34fc
amandadr <amandadroy@gmail.com>
2023-12-15 15:48:42 -0400

    remove theme by from footer

--- themes/gatsby-theme-jodie/src/components/footer.tsx
+++ themes/gatsby-theme-jodie/src/components/footer.tsx
@@ -37,23 +37,6 @@ const Footer = ({ bg }: { bg: string }) => {
       <div>
         &copy; {new Date().getFullYear()} by {siteTitle}.
       </div>
-      <div>
-        <Link
-          aria-label="Link to the theme's GitHub repository"
-          href="https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-jodie"
-        >
-          Theme
-        </Link>
-        {` `}
-        by
-        {` `}
-        <Link
-          aria-label="Link to the theme author's website"
-          href="https://www.lekoarts.de?utm_source=jodie&utm_medium=Theme"
-        >
-          LekoArts
-        </Link>
-      </div>
     </footer>
   )
 }
